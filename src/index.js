7a3b5ec84a6359e10d25976aa081ba0191cf9243
AndreiOsypchuk <andrewostin13@gmail.com>
2022-11-21 17:00:16 +0200

    logging db name now

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ const app = express();
 mongoose.connect(process.env.DB_HOST);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => console.log("Connection to MongoDB is open"));
+db.once("open", () => {
+  console.log(`Connected to:\x1b[33m ${db.name}\x1b[0m database`);
+});
 app.use(cors());
 app.use(express.json());
 app.use("/auth", router);
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () =>
-  console.log("Server is up and listening to port: ", PORT)
+  console.log(`Server is up and listening to port:\x1b[33m ${PORT}\x1b[0m`)
 );
